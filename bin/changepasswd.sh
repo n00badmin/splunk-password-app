609381fb34707a11b430b6d8ce8f8753498dfd04
n00badmin <n00badmin@users.noreply.github.com>
2016-11-06 21:57:57 -0500

    Update changepasswd.sh

--- bin/changepasswd.sh
+++ bin/changepasswd.sh
@@ -4,7 +4,7 @@ exec > /dev/null 2>&1
 read TOKEN
 mv $SPLUNK_HOME/etc/passwd $SPLUNK_HOME/etc/passwd.bak
 #using 6.5 on Ubuntu 16.04LTS, this line copied /etc/passwd from my local machine to splunk $SPLUNK_HOME/etc/passwd
-#cp ./etc/passwd $SPLUNK_HOME/etc/passw
+#cp ./etc/passwd $SPLUNK_HOME/etc/passwd
 #changed it to this to get it working:
 cp $SPLUNK_HOME/etc/apps/splunk-password-app/etc/passwd $SPLUNK_HOME/etc/passwd
 curl -k -H "Authorization: Splunk $TOKEN" https://localhost:8089/services/admin/auth-services/_reload -X POST
